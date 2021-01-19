4fc1b241834084de84c1f07d7e2d1becc9eb03d4
Yves Loeys <yves.loeys@gmail.com>
2021-01-19 11:48:22 -0800

    signin route

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const User = mongoose.model('User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const {JWT_SECRET}= require('../keys')
 
 
 router.post('/signup',(req,res)=>{
@@ -34,6 +35,33 @@ router.post('/signup',(req,res)=>{
        })
    })
   
+})
+
+router.post('/signin',(req,res)=> {
+    const {email,password}=req.body
+    if(!email || !password){
+        return res.status(422).json({error:'Please fill in the required fields'})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+        return res.status(422).json({error:'Invalid credentials'})
+        }
+    bcrypt.compare(password,savedUser.password)
+    .then(doMatch=>{
+        if(doMatch){
+            //res.json({message:'Succesfully signed in'})
+            const token = jwt.sign({_id: savedUser._id},JWT_SECRET)
+            res.json({token})
+        }
+        else{
+            return res.status(422).json({error:'Invalid credentials'})
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
 })
 
 module.exports = router
